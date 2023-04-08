720989277efca6170d1441dd75bbd384355da725
OlenaKut <cundrochka@gmail.com>
2023-04-08 22:35:42 +0200

    Added pokemon API

---
+++ homeWork.js
@@ -0,0 +1,27 @@
+let pokemonData = [];
+let image = document.getElementById("image");
+async function getPokemon() {
+  try {
+    let urlPokemon = "https://pokeapi.co/api/v2/pokemon/";
+    const response = await fetch(urlPokemon);
+    const data = await response.json();
+    pokemonData = data.results;
+    //console.log(pokemonData);
+  } catch (error) {
+    console.log(error);
+  }
+
+  pokemonData.forEach((pokemon) =>
+    console.log(`Name: ${pokemon.name}, URL: ${pokemon.url}.`)
+  );
+
+  document.getElementById("text").style.visibility = "visible";
+
+  document.getElementById("name").innerHTML = ` ${pokemonData.map(
+    getPokemonName
+  )}`;
+
+  function getPokemonName(item) {
+    return [` ${item.name}`];
+  }
+}
